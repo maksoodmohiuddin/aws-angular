978525dd70a46d228c3b65c7ff7e0c7917caf937
Maksood Mohiuddin <mamohiuddin@coca-cola.com>
2019-10-22 01:13:07 -0400

    Angular AWS Amplify reference project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import API from '@aws-amplify/api';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'aws-angular';
+
+  sendEmail() {
+    let apiName = 'restapi'; // replace this with your api name.
+    let path = '/items'; //replace this with the path you have configured on your API
+    let myInit = {
+      body: {}, // replace this with attributes you need
+      headers: {} // OPTIONAL
+    }
+
+    API.post(apiName, path, myInit).then(response => {
+      // Add your code here
+      alert('Email Sent!');
+      console.log(response);
+    }).catch(error => {
+      alert('Error sending email');
+      console.log(error.response)
+    });
+  }
+
+  
 }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import API from '@aws-amplify/api';
+import awsconfig from './aws-exports';
+
+API.configure(awsconfig);
+
 if (environment.production) {
   enableProdMode();
 }
